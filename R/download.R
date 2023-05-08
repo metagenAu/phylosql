e789c575c25b62fe37ee5d1c0969078e29e76a2f
chris <chrismbaldock@gmail.com>
2023-05-08 19:40:28 +1000

    changed connection functionality

--- R/download.R
+++ R/download.R
@@ -352,7 +352,19 @@ get_mtgn_connection<-
       return(con)
 
     }else{
-      stop("Oops! No secret key found.")
+      con<-
+        dbConnect(
+          RMariaDB::MariaDB(),
+          host= "mtgn-sh.ct3nwhqmqw5a.ap-southeast-2.rds.amazonaws.com",
+          dbname="mtgn",
+          port=3306,
+          user="metagen_admin",
+          password=keyring::key_get(
+            service = "mtgn-aws",
+            username = "metagen_admin"))
+      message("Oops! No secret key found. Attempting Keyring method.")
+      return(con)
+
     }
 
   }
